2ff2feacea2d91938fd711c0a0e28243b10d96ff
ManashHazarika <manash.p.haz@gmail.com>
2025-11-03 20:42:27 +0530

    init

---
+++ src/Calculator.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Calculator {
+    public static void main() {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the first number : ");
+        double num1 = input.nextDouble();
+        System.out.print("Enter the second number : ");
+        double num2 = input.nextDouble();
+        System.out.print("Enter the operation : ");
+        char op = input.next().charAt(0);
+
+        double total = num1 + num2;
+        System.out.println("Sum : " + total);
+    }
+}
